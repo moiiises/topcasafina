548ec9d4b6ed5cda8c17d5e348ed5f4bd48d6532
Moises__sampaio <123128851+moiiises@users.noreply.github.com>
2023-02-26 17:27:45 -0300

    coloquei os arquivos do site

---
+++ bootstrap/bootstrap-3.3.7-dist/js/navbar-animation.js
@@ -0,0 +1,6 @@
+$('#collapse-navbar').on('show.bs.collapse', function() {
+    $('.baner').css('transform', 'translate(-50%, 10%');
+ });
+$('#collapse-navbar').on('hide.bs.collapse', function() {
+    $('.baner').css('transform', 'translate(-50%, -50%');
+ });
